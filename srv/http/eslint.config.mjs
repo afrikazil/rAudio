06f6cca41df2ba786e03eb72953d13a4799561b1
k.kadyrov@ffintech.com <k.kadyrov@ffintech.com>
2024-10-08 08:19:39 +0300

    update eslint

--- srv/http/eslint.config.mjs
+++ srv/http/eslint.config.mjs
@@ -1,14 +1,11 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,vue}"]},
+  {files: ["**/*.{js,mjs,cjs,vue}"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
   ...pluginVue.configs["flat/essential"],
-  {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
 ];
